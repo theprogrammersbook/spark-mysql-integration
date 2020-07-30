d561c170fda1622ea276c2d3fe7cf2f34cbf63d1
The ProgrammersBook <theprogrammersbook@gmail.com>
2020-07-30 09:22:33 +0530

    Added project

---
+++ src/main/scala/com.tpb.spark.mysql/SparkWorking.scala
@@ -0,0 +1,17 @@
+package com.tpb.spark.mysql
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkWorking {
+  def main(args : Array[String]):Unit={
+      // Creating Spark Config
+       val sparkConfig = new SparkConf()
+         .setAppName("SparkWorking")
+         .setMaster("local")
+      // Creating Spark Context with spark config
+     val sparkContext = new SparkContext(sparkConfig)
+      // Checking AppName
+     println(sparkContext.appName)
+
+  }
+}
